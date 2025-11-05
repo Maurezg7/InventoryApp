c864dc9763b9cae199908e9b1adc7ac2f506a996
Maurezg7 <gomezmauroleonel7@gmail.com>
2025-11-05 19:43:08 -0300

    Guardar cambios actuales antes de crear service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListProduct } from "./list-product/list-product";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListProduct],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
